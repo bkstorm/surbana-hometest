49dd08f83ca999e6bf290eb75894ff75d146f993
Hoang Nguyen <hoangnv@outlook.com>
2024-12-30 18:43:08 +0700

    docs: setup swagger and update guide in README

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { LoggerInterceptor } from './location';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app
     .useGlobalPipes(new ValidationPipe({ transform: true }))
     .useGlobalInterceptors(new LoggerInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
